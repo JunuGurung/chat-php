d489c18dc087e12022dbbce34e8ed447838cedd6
Junu Gurung <junugurung48@gmail.com>
2017-01-20 13:38:10 -0500

    style and db page is created with styling

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<h21>Hello</h21>
+	<div id="container">
+		<div id="chat_box">
+			<div id="chat_data">
+				<span style="color:green">Junu: </span>
+				<span style="color: blue">How you are?</span>
+				<span style="float: right">12:30pm</span>
+			</div>
+		</div>
+		
+		<form method="post" action="index.php">
+			<input type="text" name="fullname" placeholder="Your Name">
+			<textarea name="message" placeholder="Enter Message"></textarea>
+			<input type="submit" name="submit" value="Send">
+		</form>
+	</div>
 
 </body>
 </html>
